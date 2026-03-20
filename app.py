f13f994aa30dec8eb003f8ea00ea059dbe44c12c
Twinkle Kanparia <twiinklekanparia@gmail.com>
2026-03-20 06:31:33 +0000

    initial project setup

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
- return "Hello"
+ return "Home Page"
 
-if __name__ == '__main__':
- app.run(debug=True)
+
+@app.route('/login')
+def login():
+ return "<h2>Login Page</h2>"
+
+@app.route('/register')
+def register():
+ return "<h2>Register Page</h2>"
