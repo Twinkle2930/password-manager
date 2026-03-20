7e1b041d9ae057ab9cfde60a9191ffe2b60acac0
Twinkle Kanparia <twiinklekanparia@gmail.com>
2026-03-20 14:54:27 +0000

    connected sqlite database

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 import sqlite3
 
-app = Flask(__name__)
+app = Flask(_name_)
 
-@app.route('/')
-def home():
- return "Home Page"
-
-
-@app.route('/login')
-def login():
- return "<h2>Login Page</h2>"
-
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
- return "<h2>Register Page</h2>"
- @app.route('/register', method ['GET','POST'])
- def register():
-conn = sqlite3.connect('database.db')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # database connection
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+
+        # create table
+        cursor.execute("CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT)")
+
+        # insert data
+        cursor.execute("INSERT INTO users VALUES (?, ?)", (username, password))
+
+        conn.commit()
+        conn.close()
+
+        return redirect('/login')
+
+    return render_template('register.html')
